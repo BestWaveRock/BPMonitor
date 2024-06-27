be6d38b2ec38472ae3f0fb94cb819e4a8158a7d9
liangjiaxin <liangjiaxin@e-byte.com>
2024-06-27 16:18:30 +0800

    Initial Commit

---
+++ BPMoniator/BPMoniatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPMoniatorApp.swift
+//  BPMoniator
+//
+//  Created by Yuki on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BPMoniatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
